90f93ba3e8c1deb5741324f430bdd51b4f517e50
mahfudly <mahfudlysyahputra@gmail.com>
2018-05-26 14:42:42 +0700

    First Commit

---
+++ coba/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace coba
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+			Console.WriteLine("Aku tamvan, ehehe");
+        }
+    }
+}
